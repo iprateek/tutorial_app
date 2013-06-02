e44f14ef68a3b3c813cd5f1f9a8cbb7567d97dee
Prateek <mailbox.prateek@gmail.com>
2013-06-03 01:07:52 +0530

    Drying out specs for StaticPages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe "Static Pages" do 
 
+
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
 	describe "Home Page" do
 
 		it "should have the content 'Sample App'" do
@@ -13,7 +16,7 @@ describe "Static Pages" do
 		it "should have the right title" do
 			visit '/static_pages/home'
 			page.should have_selector('title', 
-				:text =>"Ruby on Rails Tutorial Sample App | Home")
+				:text =>"#{base_title} | Home")
 		end
 	end
 
@@ -29,7 +32,7 @@ describe "Static Pages" do
 		it "should have the right title" do
 			visit '/static_pages/help'
 			page.should have_selector('title', 
-				:text =>"Ruby on Rails Tutorial Sample App | Help")
+				:text =>"#{base_title} | Help")
 		end
 	end
 
@@ -44,7 +47,7 @@ describe "Static Pages" do
 		it "should have the right title" do
 			visit '/static_pages/about'
 			page.should have_selector('title', 
-				:text =>"Ruby on Rails Tutorial Sample App | About")
+				:text =>"#{base_title} | About")
 		end
 
 	end
